b053ec1539351430d49a9ecd5c45ffb0e86ef8ff
wangdayan <wang@qq.com>
2021-12-23 00:43:38 +0800

    first commit

---
+++ src/main/java/com/wangdayan/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.wangdayan.git;
+
+/**
+ * @author WangDayan
+ * @creater 2021-12-23 0:39
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+    }
+}
